dbe4f540f5975983a98b6c1aa1300c73ab300e30
Koushik-Ramesh <koushik.r9380@gmail.com>
2024-03-19 13:51:39 +0530

    Autopush

--- main.tf
+++ main.tf
@@ -3,6 +3,12 @@ module "docdb" {
   ENV                  = var.ENV
 }
 
+module "redis" {
+  source               = "./vendor/modules/redis"
+  ENV                  = var.ENV
+}
+
+
 
 # We cannot parametrize anything that's added in the source
 # To limit that, we can use a tool called as Terrafile. All it does is, it clones the remote code fro the specified branch and make it locally available,
